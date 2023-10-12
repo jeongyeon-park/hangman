7797a29f9213f13082e5912b4cb038d2d3b52183
jeongyeon-park <dus7653@gmail.com>
2023-10-12 17:52:57 +0900

    chore

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Hangman
 
-## 배포링크
+### 1인 토이프로젝트
+
+추석 연휴 코딩 육상대회 행맨 주제
+
+[코딩육상대회](https://hanghaeplus-coyukdae.oopy.io/) / [행맨](https://hanghaeplus-coyukdae.oopy.io/)
+
+### 사용 스택
+
+- React
+- Redux
+- Tailwind CSS
+
+### 플레이 방법
+
+1. 배포링크를 클릭해 행맨 게임을 시작합니다.
+2. 랜덤으로 생성되는 한자리 ~ 여섯자리의 영단어를 추측해 맞춰보세요 !
+
+[배포링크](https://jeongyeon-park.github.io/hangman/)
--- src/pages/game/modal/index.tsx
+++ src/pages/game/modal/index.tsx
@@ -11,6 +11,7 @@ const Modal = () => {
   const answer = useSelector((state: stateType) => state.answer);
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   return (
     <div
@@ -60,7 +61,7 @@ const Modal = () => {
                 className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-2xl font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
                 onClick={() => {
                   dispatch(resetSetting());
-                  window.location.reload();
+                  navigate("/");
                 }}
               >
                 다시 도전하기
